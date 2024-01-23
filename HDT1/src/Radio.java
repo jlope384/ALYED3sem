1f7d6a3ca2a7358fa440245e7d17ef7e0b39f57d
Luis Pedro Lira <luispelirak130@gmail.com>
2024-01-23 12:21:43 -0600

    Create main content

---
+++ HDT1/src/Radio.java
@@ -0,0 +1,57 @@
+public class Radio implements IRadio{
+
+    @Override
+    public boolean getState() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getState'");
+    }
+
+    @Override
+    public void tooglePowerOffOn() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'tooglePowerOffOn'");
+    }
+
+    @Override
+    public void toogleAMFM() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toogleAMFM'");
+    }
+
+    @Override
+    public boolean getStateAMFM() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getStateAMFM'");
+    }
+
+    @Override
+    public void nextFrequency() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'nextFrequency'");
+    }
+
+    @Override
+    public void previousFrequency() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'previousFrequency'");
+    }
+
+    @Override
+    public float getCurrentFrequency() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCurrentFrequency'");
+    }
+
+    @Override
+    public void setFavFrequency(int button) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setFavFrequency'");
+    }
+
+    @Override
+    public float getFavFrequency(int button) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getFavFrequency'");
+    }
+    
+}
