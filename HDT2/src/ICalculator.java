da142de7704da59ec9d2dcf2ea1b6c15e54eb2b5
CarEsteban <car23016@uvg.edu.gt>
2024-01-25 12:01:34 -0600

    interface created

---
+++ HDT2/src/ICalculator.java
@@ -0,0 +1,10 @@
+public interface ICalculator {
+    // Definición de la interfaz ICalculator
+    public int add(int n1, int n2); // Suma
+    public int subtraction(int n1, int n2); // Resta
+    public int multiplication(int n1, int n2); // Multiplicación
+    public int division(int n1, int n2); // División
+    public int residue(int n1, int n2); // Residuo (módulo)
+}
+
+
