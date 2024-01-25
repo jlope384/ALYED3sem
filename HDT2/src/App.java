c90c23d382cc90b457483746f78ae1e504962466
jlope384 <lop23415@uvg.edu.gt>
2024-01-25 12:09:17 -0600

    xd

--- HDT2/src/App.java
+++ HDT2/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        for (int i = 0; i < 1000000000; i++) {
+            System.out.println("Hello, World!");
+        }
     }
 }
