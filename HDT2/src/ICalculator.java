10b313552d4cbe3a2359f003178d0d51adbf83a9
jlope384 <lop23415@uvg.edu.gt>
2024-01-25 12:21:19 -0600

    Interfaz actualizada

--- HDT2/src/ICalculator.java
+++ HDT2/src/ICalculator.java
@@ -1,10 +1,18 @@
+import java.util.ArrayList;
+
 public interface ICalculator {
     // Definición de la interfaz ICalculator
     public int add(int n1, int n2); // Suma
+
     public int subtraction(int n1, int n2); // Resta
+
     public int multiplication(int n1, int n2); // Multiplicación
+
     public int division(int n1, int n2); // División
+
     public int residue(int n1, int n2); // Residuo (módulo)
+
+    public ArrayList<Character> read(String CharactersString);
+
+    public int result(ArrayList<Character> elements);
 }
-
-
