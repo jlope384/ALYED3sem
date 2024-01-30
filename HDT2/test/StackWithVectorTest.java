a94c2d98c36a985e7aae0f51e7a356000f077f67
Luis Pedro Lira <luispelirak130@gmail.com>
2024-01-30 08:58:41 -0600

    StackWithVectorTest file creation

---
+++ HDT2/test/StackWithVectorTest.java
@@ -0,0 +1,29 @@
+package HDT2.test;
+import org.junit.Test;
+
+public class StackWithVectorTest {
+    @Test
+    public void testIsEmpty() {
+        
+    }
+
+    @Test
+    public void testPeek() {
+
+    }
+
+    @Test
+    public void testPop() {
+
+    }
+
+    @Test
+    public void testPush() {
+
+    }
+
+    @Test
+    public void testSize() {
+
+    }
+}
