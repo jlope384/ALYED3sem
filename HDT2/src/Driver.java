d0a2955e75deef9e850806b3ca4064b96b5d9fe8
CarEsteban <car23016@uvg.edu.gt>
2024-01-30 20:08:32 -0600

    si no funciona cambiar path a:

--- HDT2/src/Driver.java
+++ HDT2/src/Driver.java
@@ -6,7 +6,8 @@ public class Driver {
     public static void main(String[] args) throws Exception {
         IStack<String> stack = new StackWithVector<>();
         ICalculator calculator =  new Calculator();
-        String file = "HDT2\\datos.txt";
+        //SI NO FUNCIONA, INTENTAR CON PATH : "../datos.txt" o a "HDT2\\datos.txt"
+        String file = "../datos.txt";
 
         String CharactersString;
         int result=0;
