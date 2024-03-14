8b0184ecf9cb7660716b3387d38305d2bff66857
Carlos Aldana <138615730+C3AC@users.noreply.github.com>
2024-03-14 11:29:29 -0600

    Estudiante 2

---
+++ HDT6/FSHA1.java
@@ -0,0 +1,23 @@
+package HDT6;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class FSHA1 implements IHash {
+    @Override
+    public String CreateHash(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] hashBytes = md.digest(input.getBytes());
+            StringBuilder result = new StringBuilder();
+            for (byte b : hashBytes) {
+                result.append(String.format("%02x", b));
+            }
+            return result.toString();   
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
