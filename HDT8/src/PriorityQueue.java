0e6450b72a4d4188929f564aec7076dbed8482a6
Javier Lopez <lop23415@uvg.edu.gt>
2024-04-11 21:32:45 -0600

    Javadoc

--- HDT8/src/PriorityQueue.java
+++ HDT8/src/PriorityQueue.java
@@ -1,6 +1,37 @@
+/**
+ * The PriorityQueue interface represents a collection of elements that can be
+ * added and removed based on their priority. The elements are ordered in such
+ * a way that the highest priority element is always at the front of the queue.
+ *
+ * @param <T> the type of elements in the priority queue
+ */
 public interface PriorityQueue<T> {
+    /**
+     * Adds the specified element to the priority queue.
+     *
+     * @param element the element to be added
+     * @return true if the element was successfully added, false otherwise
+     */
     boolean add(T element);
+
+    /**
+     * Removes and returns the highest priority element from the priority queue.
+     *
+     * @return the highest priority element, or null if the queue is empty
+     */
     T remove();
+
+    /**
+     * Checks if the priority queue is empty.
+     *
+     * @return true if the priority queue is empty, false otherwise
+     */
     boolean isEmpty();
+
+    /**
+     * Returns the number of elements in the priority queue.
+     *
+     * @return the number of elements in the priority queue
+     */
     int size();
 }
--- HDT8/src/Proceso.java
+++ HDT8/src/Proceso.java
@@ -1,36 +1,77 @@
+/**
+ * Represents a process in a system.
+ */
 public class Proceso implements Comparable<Proceso> {
     private String nombre;
     private String usuario;
     private int nice;
 
+    /**
+     * Constructs a new Proceso object with the given nombre, usuario, and nice values.
+     * 
+     * @param nombre  the name of the process
+     * @param usuario the user of the process
+     * @param nice    the nice value of the process
+     */
     public Proceso(String nombre, String usuario, int nice) {
         this.nombre = nombre;
         this.usuario = usuario;
         this.nice = nice;
     }
 
+    /**
+     * Returns the name of the process.
+     * 
+     * @return the name of the process
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Returns the user of the process.
+     * 
+     * @return the user of the process
+     */
     public String getUsuario() {
         return usuario;
     }
 
+    /**
+     * Returns the nice value of the process.
+     * 
+     * @return the nice value of the process
+     */
     public int getNice() {
         return nice;
     }
 
+    /**
+     * Calculates and returns the priority of the process.
+     * 
+     * @return the priority of the process
+     */
     public int calcularPrioridad() {
         return 120 + nice;
     }
 
+    /**
+     * Compares this process with another process based on their priorities.
+     * 
+     * @param otro the process to compare with
+     * @return a negative integer, zero, or a positive integer if this process has a lower, equal, or higher priority than the other process
+     */
     @Override
     public int compareTo(Proceso otro) {
         // Comparar procesos por su prioridad
         return Integer.compare(calcularPrioridad(), otro.calcularPrioridad());
     }
 
+    /**
+     * Returns a string representation of the process.
+     * 
+     * @return a string representation of the process
+     */
     @Override
     public String toString() {
         return nombre + ", " + usuario + ", " + "PR:" + calcularPrioridad() + ", nice:" + nice;
