81a355c8841d1cdffcbe9eed30d7fa296697ff6b
dannyrmrz <ram23053@uvg.edu.gt>
2024-04-11 20:25:36 -0600

    Creación de la clase Proceso

---
+++ HDT8/src/Proceso.java
@@ -0,0 +1,38 @@
+public class Proceso implements Comparable<Proceso> {
+    private String nombre;
+    private String usuario;
+    private int nice;
+
+    public Proceso(String nombre, String usuario, int nice) {
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.nice = nice;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public int getNice() {
+        return nice;
+    }
+
+    public int calcularPrioridad() {
+        return 20 + nice;
+    }
+
+    @Override
+    public int compareTo(Proceso otro) {
+        // Comparar procesos por su prioridad
+        return Integer.compare(calcularPrioridad(), otro.calcularPrioridad());
+    }
+
+    @Override
+    public String toString() {
+        return nombre + ", " + usuario + ", " + "PR:" + calcularPrioridad() + ", nice:" + nice;
+    }
+}
